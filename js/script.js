7498add3bfed1a887c71f3352e7ccceb92e49c57
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-07 16:01:36 +0100

    ciclo

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 // 2 Sostituire ai divisibile per 5 buzz
 // 3 Sostituire ai divisibile per 15 fizzbuzz
 
+// prendo l'elemento dal dom
+const numberElement = document.getElementById('number')
+
+const listElement = document.createElement('ul')
+
+// creo il ciclo per i 100 numeri
+for (let i = 1; i <= 100; i++) {
+
+}
+
