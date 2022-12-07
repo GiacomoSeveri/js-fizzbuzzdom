3f9f0f23c66982572d7c34c86b463bbaed6afbe6
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-07 16:01:48 +0100

    prove

--- js/script.js
+++ js/script.js
@@ -10,6 +10,20 @@ const listElement = document.createElement('ul')
 
 // creo il ciclo per i 100 numeri
 for (let i = 1; i <= 100; i++) {
-
+    console.log (i);
+    const listItem = document.createElement('li');
+    // sostituisco i divisibili per 3
+    if (i % 3 === 0) {
+        listItem.append("fizz ");
+        listElement.appendChild(listItem);
+    };
+    // sostituisco i divisibili per 5
+    // if (i % 5 === 0) {
+        
+    // };
+    // sostituisco i divisibili per 15
+    // if (i % 15 === 0) {
+        
+    // };
 }
 
